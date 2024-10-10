0230bdc98bbc0bf767709b13529ff31a76746c05
venta <dinergarciacondori4@gmail.com>
2024-10-10 11:15:08 -0500

    actualizar

--- views/perfil.php
+++ views/perfil.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
-</head>
+
 <body>
 
     <style>
--- views/registrate.php
+++ views/registrate.php
@@ -1,10 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>REGISTRATE</title>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap.min.css">
   <style>
 .container.my-5 {
   margin-top: 3rem !important;
@@ -176,5 +170,3 @@
       window.location.href = 'login.html';
     });
   </script>
-</body>
-</html>
