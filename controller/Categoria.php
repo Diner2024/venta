688f667779f82a80a6e1310eec08aa8d99febcff
AlexisXPL12 <valdiviatruciosalexisgabriel@gmail.com>
2024-10-25 08:22:44 -0500

    dfsd

--- controller/Categoria.php
+++ controller/Categoria.php
@@ -2,15 +2,15 @@
 require_once('../model/categoriaModel.php');
 $tipo = $_REQUEST['tipo'];
 
-//instanciar la clase categoria  model
-$objCategoria = new categoriaModel();
+//instanciar la clase categoria model
+$objCategoria = new CategoriaModel();
 
-
-if ($tipo=="Listar"){
+if ($tipo=="listar") {
     //respuesta
-    $arr_Respuesta = array ('status'==false, 'contenido'=>'');
-    $arr_Categorias = $objCategoria->obtener_categoria();
+    $arr_Respuesta = array('status'=>false, 'contenido'=>'');
+    $arr_Categorias = $objCategoria->obtener_categorias();
 
     print_r($arr_Categorias);
 }
+
 ?>
--- model/categoriaModel.php
+++ model/categoriaModel.php
@@ -1,5 +1,5 @@
 <?php
-require_once "../libreria/conexion.php";
+require_once "../libreria/conexcion.php";
 class CategoriaModel{
 
     private $conexion;
