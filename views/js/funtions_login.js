6320a26ecb50d3fa8cb9cb9455fed4a4163cc02b
AlexisXPL12 <valdiviatruciosalexisgabriel@gmail.com>
2024-12-10 09:38:08 -0500

    09:38

--- views/js/funtions_login.js
+++ views/js/funtions_login.js
@@ -1,4 +1,4 @@
-async function iniciar_sesion() {
+async function iniciar_sesion(params) {
     let usuario = document.querySelector('#usuario');
     let password = document.querySelector('#password');
     if (usuario=="" || password =="") {
@@ -6,39 +6,39 @@ async function iniciar_sesion() {
         return;
     }
     try {
-        // capturamos datos del formulario html
-        const datos = new FormData(frm_iniciar_sesion);
+        //capturamos datos del formulario html
+        const datos = new FormData(loginForm);
         //enviar datos hacia el controlador
         let respuesta = await fetch(base_url + 'controller/login.php?tipo=iniciar_sesion', {
             method: 'POST',
             mode: 'cors',
-            cache: 'no-cache',
+            cahe: 'no-cache',
             body: datos
         });
         json = await respuesta.json();
         if (json.status) {
-            //swal("Iniciar Sesion", json.mensaje, "success");
-            location.replace(base_url+"inicio");
+           // swal("iniciar_sesion", json.mensaje, "success");
+           location.replace(base_url+"inicio")
         } else {
-            swal("Iniciar Sesion", json.mensaje, "error");
+            swal("iniciar_sesion", json.mensaje, "error");
         }
 
         console.log(json);
     } catch (e) {
-        console.log("Oops, ocurrio un error " + e);
+        console.log("Oops ocurrio un error" + e);
     }
-    
 }
 
-if (document.querySelector('#frm_iniciar_sesion')) {
-    // evita que se envie el formulario
-    let frm_iniciar_sesion = document.querySelector('#frm_iniciar_sesion');
-    frm_iniciar_sesion.onsubmit = function (e) {
+
+if (document.querySelector('#loginForm')){
+    //evita que se envie el forulario
+    let loginForm = document.querySelector
+    ('#loginForm');
+    loginForm.onsubmit = function (e){
         e.preventDefault();
         iniciar_sesion();
     }
 }
-
 
 async function  cerrar_sesion() {
     try{
